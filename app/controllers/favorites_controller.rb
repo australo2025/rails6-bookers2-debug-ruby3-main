482bb5700b456a26b50f83fcd54183f30e3d9997
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-09-29 14:28:58 +0000

    “いいねを非同期化”

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,20 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    current_user.favorites.create!(book_id: book.id)
-    redirect_back fallback_location: book_path(book)
+    @book = Book.find(params[:book_id])
+    current_user.favorites.create!(book_id: @book.id)
+    respond_to do |format|                                      # ← 追加
+      format.html { redirect_back fallback_location: book_path(@book) }
+      format.js
   end
+end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by!(book_id: book.id)
-    favorite.destroy
-    redirect_back fallback_location: book_path(book)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by!(book_id: @book.id).destroy
+    respond_to do |format|                                      # ← 追加
+      format.html { redirect_back fallback_location: book_path(@book) }
+      format.js
   end
+end
 end
