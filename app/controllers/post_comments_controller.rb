5d6e52cb48eb143b8a52308bcabb61b2946821dc
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-09-30 03:52:05 +0000

    “コメントの非同期化”

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,29 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(post_comment_params.merge(book: @book))
+
+    if @post_comment.save
+      respond_to do |format|
+        format.html { redirect_to book_path(@book) }
+        format.js   # ← create.js.erb を返す
+      end
+    else
+      # 失敗時もひとまず同期遷移でOK（簡易）
+      redirect_to book_path(@book)
+    end
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.find(params[:id])  # 簡易。自分のだけにしたいなら current_user.post_comments.find
+    @post_comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to book_path(@book) }
+      format.js   # ← destroy.js.erb を返す
+    end
   end
 
   private
