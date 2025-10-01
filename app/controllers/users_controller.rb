f359b655aeedb8807e4b838237e18f086a59da76
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-10-01 05:36:02 +0000

    グラフ機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,16 @@ class UsersController < ApplicationController
     @count_yesterday  = @user.books.where(created_at: yesterday_range).count
     @count_this_week  = @user.books.where(created_at: this_week_range).count
     @count_last_week  = @user.books.where(created_at: last_week_range).count
+    from = 6.days.ago.beginning_of_day
+    to   = Time.current.end_of_day
+
+    dates = (from.to_date..Date.current).to_a
+    counts_by_date = @user.books.where(created_at: from..to)
+                         .pluck(:created_at)                 # DBから時刻を取り出し
+                         .map { |t| t.in_time_zone.to_date } # RailsのTZで日付に変換
+                         .tally                               # {Date=>件数}
+    @counts = dates.map { |d| counts_by_date[d] || 0 }
+    @labels = dates.map { |d| d == Date.current ? "今日" : "#{(Date.current - d).to_i}日前" }
   end
 
   def index
